d5c5219f7830295e9cbf665c455f6bb1d835ff4b
Ricky Singh <Gurjinder.Singh.Ricky@protonmail.com>
2025-04-17 04:58:54 -0400

    fix: null current user after restarting app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:slice_quest/features/auth/controller/auth_controller.dart';
 import 'package:slice_quest/features/auth/screens/welcome_screen.dart';
-import 'package:slice_quest/features/quests/screens/all_quests_screen.dart';
+import 'package:slice_quest/features/quests/controller/quest_controller.dart';
 import 'package:slice_quest/features/quests/screens/tab_screen.dart';
 import 'package:slice_quest/firebase_options.dart';
 import 'package:slice_quest/providers/firebase_providers.dart';
@@ -24,6 +25,7 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.read(questControllerProvider.notifier).getQuestData();
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData.dark(),
@@ -32,6 +34,12 @@ class MyApp extends ConsumerWidget {
             stream: ref.watch(authProvider).authStateChanges(),
             builder: (ctx, snapshot) {
               if (snapshot.hasData) {
+                final email = snapshot.data!.email;
+                if (ref.read(currentUserProvider) == null) {
+                  ref
+                      .watch(authControllerProvider.notifier)
+                      .getUserViaEmail(email!);
+                }
                 return const TabScreen();
               }
               return const WelcomeScreen();
