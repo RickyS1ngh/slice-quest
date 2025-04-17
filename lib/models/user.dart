c8a93a16205a55aff1cbb3d0a462308f63dc885f
Ricky Singh <Gurjinder.Singh.Ricky@protonmail.com>
2025-04-17 05:00:58 -0400

    feat: add email field to usermode

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,56 +1,84 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
 class UserModel {
+  final String email;
   final String username;
   final String uid;
   final String profileImage;
-  final List completedQuests;
+  final List<String> completedQuests;
   final int xp;
+  final String activeQuest;
   UserModel({
+    required this.email,
     required this.username,
     required this.uid,
     required this.profileImage,
     required this.completedQuests,
     required this.xp,
+    required this.activeQuest,
   });
 
   UserModel copyWith({
+    String? email,
     String? username,
-    String? uuid,
+    String? uid,
     String? profileImage,
-    List? completedQuests,
+    List<String>? completedQuests,
     int? xp,
+    String? activeQuest,
   }) {
     return UserModel(
+      email: email ?? this.email,
       username: username ?? this.username,
-      uid: uuid ?? this.uid,
+      uid: uid ?? this.uid,
       profileImage: profileImage ?? this.profileImage,
       completedQuests: completedQuests ?? this.completedQuests,
       xp: xp ?? this.xp,
+      activeQuest: activeQuest ?? this.activeQuest,
     );
   }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
+      'email': email,
       'username': username,
       'uid': uid,
       'profileImage': profileImage,
       'completedQuests': completedQuests,
       'xp': xp,
+      'activeQuest': activeQuest,
     };
+  }
+
+  factory UserModel.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    final data = doc.data();
+    return UserModel(
+        email: data!['email'],
+        username: data['username'],
+        uid: doc.id,
+        profileImage: data['profileImage'],
+        completedQuests: (data['completedQuests'] as List<dynamic>?)
+                ?.map((e) => e.toString())
+                .toList() ??
+            [],
+        xp: data['xp'],
+        activeQuest: data['activeQuest'] ?? null);
   }
 
   factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
+      email: map['email'] as String,
       username: map['username'] as String,
       uid: map['uid'] as String,
       profileImage: map['profileImage'] as String,
       completedQuests: List.from(map['completedQuests'] as List),
       xp: map['xp'] as int,
+      activeQuest: map['activeQuest'] as String,
     );
   }
 
@@ -61,26 +89,30 @@ class UserModel {
 
   @override
   String toString() {
-    return 'UserModel(username: $username, uuid: $uid, profileImage: $profileImage, completedQuests: $completedQuests, xp: $xp)';
+    return 'UserModel(email: $email, username: $username, uid: $uid, profileImage: $profileImage, completedQuests: $completedQuests, xp: $xp, activeQuest: $activeQuest)';
   }
 
   @override
   bool operator ==(covariant UserModel other) {
     if (identical(this, other)) return true;
 
-    return other.username == username &&
+    return other.email == email &&
+        other.username == username &&
         other.uid == uid &&
         other.profileImage == profileImage &&
         listEquals(other.completedQuests, completedQuests) &&
-        other.xp == xp;
+        other.xp == xp &&
+        other.activeQuest == activeQuest;
   }
 
   @override
   int get hashCode {
-    return username.hashCode ^
+    return email.hashCode ^
+        username.hashCode ^
         uid.hashCode ^
         profileImage.hashCode ^
         completedQuests.hashCode ^
-        xp.hashCode;
+        xp.hashCode ^
+        activeQuest.hashCode;
   }
 }
