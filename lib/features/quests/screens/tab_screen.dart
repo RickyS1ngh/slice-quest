5d1b0a5f506881c9bfe142e58ce5ce3bb71c3795
Ricky Singh <Gurjinder.Singh.Ricky@protonmail.com>
2025-04-18 22:01:49 -0400

    feat: update tab screen app bar

--- lib/features/quests/screens/tab_screen.dart
+++ lib/features/quests/screens/tab_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:slice_quest/features/auth/controller/auth_controller.dart';
 import 'package:slice_quest/features/quests/screens/all_quests_screen.dart';
 import 'package:slice_quest/features/quests/screens/completed_quests_screen.dart';
 
@@ -15,19 +15,36 @@ class _TabScreenState extends ConsumerState<TabScreen> {
   int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
+    var profileImage =
+        ref.read(currentUserProvider)?.profileImage ?? 'pizza_user_avatar';
     return Scaffold(
+        appBar: AppBar(
+            leading: Image.asset('assets/images/pizza.png'),
+            title: const Text(
+              'Slice Quest',
+              style: TextStyle(
+                fontFamily: 'Pizzaman',
+              ),
+            ),
+            actions: [
+              CircleAvatar(
+                  backgroundImage:
+                      Image.asset('assets/images/$profileImage.png').image)
+            ]),
         bottomNavigationBar: BottomNavigationBar(
-          items: const [
+          items: [
             BottomNavigationBarItem(
                 icon: ImageIcon(
-                  AssetImage('assets/images/scroll_icon.png'),
+                  const AssetImage('assets/images/scroll_icon.png'),
+                  color: ThemeData().colorScheme.primary,
                   size: 30,
                 ),
                 label: ''),
             BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.check_circle_outline_sharp,
-                  size: 30,
+                icon: ImageIcon(
+                  const AssetImage('assets/images/pizza_complete_icon.png'),
+                  color: ThemeData().colorScheme.primary,
+                  size: 35,
                 ),
                 label: ''),
           ],
